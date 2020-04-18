64de8b625bcfb9bfed6169266e2e526234d9e5a2
Amirmahdi80-art <62158288+Amirmahdi80-art@users.noreply.github.com>
2020-04-18 12:08:37 +0430

    Add files via upload

---
+++ home1.js
@@ -0,0 +1,17 @@
+
+let yourName = prompt("What is your name?");
+if(yourName === "amir"){
+  alert("Hi Punishedsnake7, welcome to dadachia web page.");
+}
+if(yourName === "amir mohammad"){
+  alert("Hi Juju, welcome to dadachia web page.");
+}
+if(yourName === "ali"){
+  alert("Hi AliJoonDoost, welcome to dadachia web page.");
+}
+if(yourName === "hadi"){
+  alert("Hi Hindi, welcome to dadachia web page.");
+}
+if(yourName === "pooya"){
+  alert("Hi Sheikh, welcome to dadachia web page.");
+}
